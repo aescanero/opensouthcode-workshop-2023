77641a7e16c2d5565aa5b5989e14735a19dc72d4
Alejandro Escanero Blanco <aescanero@gmail.com>
2023-05-28 21:43:19 +0200

    correct chmod

--- docker/generate_certs.sh
+++ docker/generate_certs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo chown o+rwx config
+sudo chmod o+rwx config
 sudo docker run -v ./config:/config --name micropki --rm -it docker.io/aescanero/micropki:0.1.2-linux-amd64 \
   cert new --cafile "/config/ca.crt" --cakeyfile "/config/ca.key" \
   --certfile "/config/ldap.crt" --certkeyfile "/config/ldap.key" \
@@ -9,4 +9,4 @@ sudo docker run -v ./config:/config --name micropki --rm -it docker.io/aescanero
   cert new --cafile "/config/ca.crt" --cakeyfile "/config/ca.key" \
   --certfile "/config/nginx.crt" --certkeyfile "/config/nginx.key" \
   --hosts *,*.disasterproject.com,localhost,127.0.0.1
-#sudo chown o-w config
+#sudo chmod o-w config
