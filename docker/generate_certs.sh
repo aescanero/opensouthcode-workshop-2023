14443fbea922fbb40aeaca5e00de194e82f563ca
Alejandro Escanero Blanco <aescanero@gmail.com>
2023-05-29 17:40:20 +0200

    Correct docker invocation

--- docker/generate_certs.sh
+++ docker/generate_certs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 sudo chmod o+rwx config
 sudo mkdir config/ssl
@@ -7,12 +7,12 @@ sudo mkdir ldap var_ldap
 sudo chmod 777 ldap
 sudo chmod 777 var_ldap
 sudo < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32} >config/passfile
-sudo chmod 555 config/passfile 
-sudo docker run -v ./config:/config --name micropki --rm -it docker.io/aescanero/micropki:0.1.2-linux-amd64 \
+sudo chmod 444 config/passfile 
+sudo docker run -v ./config:/config --name micropki --rm docker.io/aescanero/micropki:0.1.2-linux-amd64 \
   cert new --cafile "/config/ca.crt" --cakeyfile "/config/ca.key" \
   --certfile "/config/ldap.crt" --certkeyfile "/config/ldap.key" \
   --hosts *,*.disasterproject.com,localhost,127.0.0.1
-sudo docker run -v ./config:/config --name micropki --rm -it docker.io/aescanero/micropki:0.1.2-linux-amd64 \
+sudo docker run -v ./config:/config --name micropki --rm docker.io/aescanero/micropki:0.1.2-linux-amd64 \
   cert new --cafile "/config/ca.crt" --cakeyfile "/config/ca.key" \
   --certfile "/config/ssl/self.cert" --certkeyfile "/config/ssl/self-ssl.key" \
   --hosts *,*.disasterproject.com,localhost,127.0.0.1
