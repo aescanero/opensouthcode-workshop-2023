daa010ee0b5282d5f930a1d7dc7bb5e2120d3101
Alejandro Escanero Blanco <aescanero@gmail.com>
2023-05-30 23:18:56 +0200

    Create env files

--- docker/generate_certs.sh
+++ docker/generate_certs.sh
@@ -7,6 +7,10 @@ sudo mkdir ldap var_ldap
 sudo chmod 777 ldap
 sudo chmod 777 var_ldap
 sudo < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32} >config/passfile
+echo "LDAP_SEARCH_BIND_PASSWORD=$(cat config/passfile)" >> .env
+POSTGRES_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
+echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> .env
+echo "PASSWORD=$POSTGRES_PASSWORD" >> .env
 sudo chmod 444 config/passfile 
 sudo docker run -v ./config:/config --name micropki --rm docker.io/aescanero/micropki:0.1.2-linux-amd64 \
   cert new --cafile "/config/ca.crt" --cakeyfile "/config/ca.key" \
